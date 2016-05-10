26b59337dcf4e053b5dbe2c8853b16ec514c1a9f
aoshiman <men.katame.nori.oome@gmail.com>
2016-05-10 12:33:56 +0900

    Fix logger.py

--- logger.py
+++ logger.py
@@ -7,7 +7,7 @@ def logging(func):
     """A decorator that logs the activity of the script."""
     @wraps(func)
     def wrapper(obj, *args, **kwargs):
-        print("{0:%Y-%m-%d %H:%M:%S} {1} {2} {3} args={4} kwds={5}"\
+        print("{0:%Y-%m-%d %H:%M:%S} {1} {2} {3} args={4} kwargs={5}"\
                 .format(datetime.now(), "INFO", func.__qualname__, "START", args, kwargs))
         result = func(obj, *args, **kwargs)
         print("{0:%Y-%m-%d %H:%M:%S} {1} {2} {3} return {4}"\
